87c1eb289e2bda6c12800b2cecfb764bf09e58ec
H <pur3miish@me.com>
2021-11-15 06:41:36 +1000

    Removed dynamic imports.

--- public/sha256.js
+++ public/sha256.js
@@ -1,4 +1,5 @@
 'use strict'
+const { createHash } = require('crypto')
 
 /**
  * Universal sha256 message digest helper function.
@@ -28,12 +29,9 @@ async function sha256(data) {
   if (!(data instanceof Uint8Array))
     throw new TypeError('Expected Uint8Array input data.')
 
-  if (typeof window == 'undefined') {
-    // https://github.com/mysticatea/eslint-plugin-node/issues/250
-    // eslint-disable-next-line
-    const { createHash } = await import('crypto')
+  if (typeof window == 'undefined')
     return new Uint8Array(createHash('sha256').update(data).digest())
-  } else return new Uint8Array(await crypto.subtle.digest('SHA-256', data))
+  else return new Uint8Array(await crypto.subtle.digest('SHA-256', data))
 }
 
 module.exports = sha256
